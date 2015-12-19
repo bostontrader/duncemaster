3aa238a49e92e664799fe4bff97edf4aa57d8b51
tradloff <bostontrader@gmail.com>
2015-12-19 09:17:11 +0800

    Update Majors testing.

--- tests/Fixture/MajorsFixture.php
+++ tests/Fixture/MajorsFixture.php
@@ -4,27 +4,11 @@ namespace App\Test\Fixture;
 class MajorsFixture extends DMFixture {
     public $import = ['table' => 'majors'];
 
-    // These records are injected into the db before the tests.  We need to specify the
-    // id to ensure the test records are properly related.
-    public $major1Record = [
-        'id'=>FixtureConstants::major1_id,
-        'title' => 'Lion Taming', 'sdesc' => 'LT'
-    ];
-
-    public $major2Record = [
-        'id'=>FixtureConstants::major2_id,
-        'title' => 'Cat Juggling', 'sdesc' => 'CJ'
-    ];
-
     // This record will be added during a test.  We don't need or want to control the id here, so omit it.
     public $newMajorRecord = ['title' => 'Advanced Lion Taming', 'sdesc' => 'AT'];
 
     public function init() {
-        $this->records = [
-            $this->major1Record,
-            $this->major2Record,
-        ];
-        parent::init();
+        $this->tableName='Majors';
+        parent::init(); // This is where the records are loaded.
     }
-
 }
--- tests/TestCase/Controller/MajorsControllerTest.php
+++ tests/TestCase/Controller/MajorsControllerTest.php
@@ -17,6 +17,7 @@ class MajorsControllerTest extends DMIntegrationTestCase {
     /* @var \App\Model\Table\MajorsTable */
     private $majors;
 
+    /* @var \App\Test\Fixture\MajorsFixture */
     private $majorsFixture;
 
     public function setUp() {
@@ -39,176 +40,138 @@ class MajorsControllerTest extends DMIntegrationTestCase {
 
     public function testAddGET() {
 
-        // 1. Simulate login, submit request, examine response.
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->get('/majors/add');
-        $this->assertResponseOk(); // 2xx
-        $this->assertNoRedirect();
+        // 1. Login, GET the url, parse the response and send it back.
+        $html=$this->loginRequestResponse(FixtureConstants::userAndyAdminId,'/majors/add');
 
-        // 2. Parse the html from the response
-        $html = str_get_html($this->_response->body());
+        // 2. Ensure that the correct form exists
+        /* @var \simple_html_dom_node $form */
+        $form = $html->find('form#MajorAddForm',0);
+        $this->assertNotNull($form);
 
-        // 3. Ensure that the correct form exists
-        $this->form = $html->find('form#MajorAddForm',0);
-        $this->assertNotNull($this->form);
-
-        // 4. Now inspect the fields on the form.  We want to know that:
+        // 3. Now inspect the fields on the form.  We want to know that:
         // A. The correct fields are there and no other fields.
         // B. The fields have correct values. This includes verifying that select
         //    lists contain options.
         //
         //  The actual order that the fields are listed is hereby deemed unimportant.
 
-        // 4.1 These are counts of the select and input fields on the form.  They
+        // 3.1 These are counts of the select and input fields on the form.  They
         // are presently unaccounted for.
-        $unknownSelectCnt = count($this->form->find('select'));
-        $unknownInputCnt = count($this->form->find('input'));
+        $unknownSelectCnt = count($form->find('select'));
+        $unknownInputCnt = count($form->find('input'));
 
-        // 4.2 Look for the hidden POST input
-        if($this->lookForHiddenInput($this->form)) $unknownInputCnt--;
+        // 3.2 Look for the hidden POST input
+        if($this->lookForHiddenInput($form)) $unknownInputCnt--;
 
-        // 4.3 Ensure that there's an input field for title, of type text, and that it is empty
-        if($this->inputCheckerA($this->form,'input#MajorTitle')) $unknownInputCnt--;
+        // 3.3 Ensure that there's an input field for title, of type text, and that it is empty
+        if($this->inputCheckerA($form,'input#MajorTitle')) $unknownInputCnt--;
 
-        // 4.4 Ensure that there's an input field for sdesc, of type text, and that it is empty
-        if($this->inputCheckerA($this->form,'input#MajorSDesc')) $unknownInputCnt--;
+        // 3.4 Ensure that there's an input field for sdesc, of type text, and that it is empty
+        if($this->inputCheckerA($form,'input#MajorSDesc')) $unknownInputCnt--;
 
-        // 4.9 Have all the input and select fields been accounted for?  Are there
-        // any extras?
-        $this->assertEquals(0, $unknownInputCnt);
-        $this->assertEquals(0, $unknownSelectCnt);
-
-        // 5. Examine the <A> tags on this page.  There should be zero links.
-        $this->content = $html->find('div#MajorsAdd',0);
-        $this->assertNotNull($this->content);
-        $links = $this->content->find('a');
-        $this->assertEquals(0,count($links));
+        // 4. Have all the input, select, and Atags been accounted for?
+        $this->expectedInputsSelectsAtagsFound($unknownInputCnt, $unknownSelectCnt, $html, 'div#MajorsAdd');
     }
 
     public function testAddPOST() {
 
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->post('/majors/add', $this->majorsFixture->newMajorRecord);
-        $this->assertResponseSuccess(); // 2xx, 3xx
-        $this->assertRedirect( '/majors' );
+        // 1. Login, POST a suitable record to the url, redirect, and return the record just
+        // posted, as read from the db.
+        $fixtureRecord=$this->majorsFixture->newMajorRecord;
+        $fromDbRecord=$this->genericAddPostProlog(
+            FixtureConstants::userAndyAdminId,
+            '/majors/add', $fixtureRecord,
+            '/majors', $this->majors
+        );
 
-        // Now verify what we think just got written
-        $new_id = count($this->majorsFixture->records) + 1;
-        $query = $this->majors->find()->where(['id' => $new_id]);
-        $this->assertEquals(1, $query->count());
-
-        // Now retrieve that 1 record and compare to what we expect
-        $new_major = $this->majors->get($new_id);
-        $this->assertEquals($new_major['title'],$this->majorsFixture->newMajorRecord['title']);
-        $this->assertEquals($new_major['sdesc'],$this->majorsFixture->newMajorRecord['sdesc']);
+        // 2. Now validate that record.
+        $this->assertEquals($fromDbRecord['title'],$fixtureRecord['title']);
+        $this->assertEquals($fromDbRecord['sdesc'],$fixtureRecord['sdesc']);
     }
 
     public function testDeletePOST() {
 
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $major_id = $this->majorsFixture->major1Record['id'];
-        $this->post('/majors/delete/' . $major_id);
-        $this->assertResponseSuccess(); // 2xx, 3xx
-        $this->assertRedirect( '/majors' );
-
-        // Now verify that the record no longer exists
-        $query = $this->majors->find()->where(['id' => $major_id]);
-        $this->assertEquals(0, $query->count());
+        $major_id = $this->majorsFixture->records[0]['id'];
+        $this->deletePOST(
+            FixtureConstants::userAndyAdminId, '/majors/delete/',
+            $major_id, '/majors', $this->majors
+        );
     }
 
     public function testEditGET() {
 
-        // 1. Simulate login, submit request, examine response.
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->get('/majors/edit/' . $this->majorsFixture->major1Record['id']);
-        $this->assertResponseOk(); // 2xx
-        $this->assertNoRedirect();
+        // 1. Obtain a record to edit, login, GET the url, parse the response and send it back.
+        $record2Edit=$this->majorsFixture->records[0];
+        $url='/majors/edit/' . $record2Edit['id'];
+        $html=$this->loginRequestResponse(FixtureConstants::userAndyAdminId,$url);
 
-        // 2. Parse the html from the response
-        $html = str_get_html($this->_response->body());
+        // 2. Ensure that the correct form exists
+        /* @var \simple_html_dom_node $form */
+        $form = $html->find('form#MajorEditForm',0);
+        $this->assertNotNull($form);
 
-        // 3. Ensure that the correct form exists
-        $this->form = $html->find('form#MajorEditForm',0);
-        $this->assertNotNull($this->form);
-
-        // 4. Now inspect the fields on the form.  We want to know that:
+        // 3. Now inspect the fields on the form.  We want to know that:
         // A. The correct fields are there and no other fields.
         // B. The fields have correct values. This includes verifying that select
         //    lists contain options.
         //
         //  The actual order that the fields are listed on the form is hereby deemed unimportant.
 
-        // 4.1 These are counts of the select and input fields on the form.  They
+        // 3.1 These are counts of the select and input fields on the form.  They
         // are presently unaccounted for.
-        $unknownSelectCnt = count($this->form->find('select'));
-        $unknownInputCnt = count($this->form->find('input'));
+        $unknownSelectCnt = count($form->find('select'));
+        $unknownInputCnt = count($form->find('input'));
 
-        // 4.2 Look for the hidden POST input
-        if($this->lookForHiddenInput($this->form,'_method','PUT')) $unknownInputCnt--;
+        // 3.2 Look for the hidden POST input
+        if($this->lookForHiddenInput($form,'_method','PUT')) $unknownInputCnt--;
 
-        // 4.3 Ensure that there's an input field for title, of type text, and that it is correctly set
-        if($this->inputCheckerA($this->form,'input#MajorTitle',
-            $this->majorsFixture->major1Record['title'])) $unknownInputCnt--;
+        // 3.3 Ensure that there's an input field for title, of type text, and that it is correctly set
+        if($this->inputCheckerA($form,'input#MajorTitle',
+            $record2Edit['title'])) $unknownInputCnt--;
         
-        // 4.4 Ensure that there's an input field for sdesc, of type text, and that that it is correctly set
-        if($this->inputCheckerA($this->form,'input#MajorSDesc',
-            $this->majorsFixture->major1Record['sdesc'])) $unknownInputCnt--;
+        // 3.4 Ensure that there's an input field for sdesc, of type text, and that that it is correctly set
+        if($this->inputCheckerA($form,'input#MajorSDesc',
+            $record2Edit['sdesc'])) $unknownInputCnt--;
 
-        // 4.9 Have all the input and select fields been accounted for?  Are there
-        // any extras?
-        $this->assertEquals(0, $unknownInputCnt);
-        $this->assertEquals(0, $unknownSelectCnt);
-
-        // 5. Examine the <A> tags on this page.  There should be zero links.
-        $this->content = $html->find('div#MajorsEdit',0);
-        $this->assertNotNull($this->content);
-        $links = $this->content->find('a');
-        $this->assertEquals(0,count($links));
+        // 4. Have all the input, select, and Atags been accounted for?
+        $this->expectedInputsSelectsAtagsFound($unknownInputCnt, $unknownSelectCnt, $html, 'div#MajorsEdit');
     }
 
     public function testEditPOST() {
 
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $major_id = $this->majorsFixture->major1Record['id'];
-        $this->put('/majors/edit/' . $major_id, $this->majorsFixture->newMajorRecord);
-        $this->assertResponseSuccess(); // 2xx, 3xx
-        $this->assertRedirect('/majors');
+        // 1. Login, POST a suitable record to the url, redirect, and return the record just
+        // posted, as read from the db.
+        $fixtureRecord=$this->majorsFixture->newMajorRecord;
+        $fromDbRecord=$this->genericEditPutProlog(
+            FixtureConstants::userAndyAdminId,
+            '/majors/edit', $fixtureRecord,
+            '/majors', $this->majors
+        );
 
-        // Now verify what we think just got written
-        $query = $this->majors->find()->where(['id' => $major_id]);
-        $this->assertEquals(1, $query->count());
-
-        // Now retrieve that 1 record and compare to what we expect
-        $major = $this->majors->get($major_id);
-        $this->assertEquals($major['title'],$this->majorsFixture->newMajorRecord['title']);
-        $this->assertEquals($major['sdesc'],$this->majorsFixture->newMajorRecord['sdesc']);
+        // 2. Now validate that record.
+        $this->assertEquals($fromDbRecord['title'],$fixtureRecord['title']);
+        $this->assertEquals($fromDbRecord['sdesc'],$fixtureRecord['sdesc']);
     }
 
     public function testIndexGET() {
 
-        // 1. Simulate login, submit request, examine response.
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->get('/majors/index');
-        $this->assertResponseOk(); // 2xx
-        $this->assertNoRedirect();
+        // 1. Login, GET the url, parse the response and send it back.
+        $html=$this->loginRequestResponse(FixtureConstants::userAndyAdminId,'/majors/index');
 
-        // 2. Parse the html from the response
-        $html = str_get_html($this->_response->body());
-
-        // 3. Get a the count of all <A> tags that are presently unaccounted for.
+        // 2. Get a the count of all <A> tags that are presently unaccounted for.
         $this->content = $html->find('div#MajorsIndex',0);
         $this->assertNotNull($this->content);
         $unknownATag = count($this->content->find('a'));
 
-        // 4. Look for the create new major link
+        // 3. Look for the create new major link
         $this->assertEquals(1, count($html->find('a#MajorAdd')));
         $unknownATag--;
 
-        // 5. Ensure that there is a suitably named table to display the results.
+        // 4. Ensure that there is a suitably named table to display the results.
         $this->table = $html->find('table#majors',0);
         $this->assertNotNull($this->table);
 
-        // 6. Ensure that said table's thead element contains the correct
+        // 5. Ensure that said table's thead element contains the correct
         //    headings, in the correct order, and nothing else.
         $this->thead = $this->table->find('thead',0);
         $thead_ths = $this->thead->find('tr th');
@@ -219,13 +182,13 @@ class MajorsControllerTest extends DMIntegrationTestCase {
         $column_count = count($thead_ths);
         $this->assertEquals($column_count,3); // no other columns
 
-        // 7. Ensure that the tbody section has the same
+        // 6. Ensure that the tbody section has the same
         //    quantity of rows as the count of major records in the fixture.
         $this->tbody = $this->table->find('tbody',0);
         $tbody_rows = $this->tbody->find('tr');
         $this->assertEquals(count($tbody_rows), count($this->majorsFixture->records));
 
-        // 8. Ensure that the values displayed in each row, match the values from
+        // 7. Ensure that the values displayed in each row, match the values from
         //    the fixture.  The values should be presented in a particular order
         //    with nothing else thereafter.
         $iterator = new \MultipleIterator();
@@ -240,7 +203,7 @@ class MajorsControllerTest extends DMIntegrationTestCase {
             $this->assertEquals($fixtureRecord['title'],  $htmlColumns[0]->plaintext);
             $this->assertEquals($fixtureRecord['sdesc'],  $htmlColumns[1]->plaintext);
 
-            // 8.2 Now examine the action links
+            // 7.2 Now examine the action links
             $this->td = $htmlColumns[2];
             $actionLinks = $this->td->find('a');
             $this->assertEquals('MajorView', $actionLinks[0]->name);
@@ -250,29 +213,26 @@ class MajorsControllerTest extends DMIntegrationTestCase {
             $this->assertEquals('MajorDelete', $actionLinks[2]->name);
             $unknownATag--;
 
-            // 8.9 No other columns
+            // 7.9 No other columns
             $this->assertEquals(count($htmlColumns),$column_count);
         }
 
-        // 9. Ensure that all the <A> tags have been accounted for
+        // 8. Ensure that all the <A> tags have been accounted for
         $this->assertEquals(0, $unknownATag);
     }
 
     public function testViewGET() {
 
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->get('/majors/view/' . $this->majorsFixture->major1Record['id']);
-        $this->assertResponseOk(); // 2xx
-        $this->assertNoRedirect();
+        // 1. Obtain a record to view, login, GET the url, parse the response and send it back.
+        $record2View=$this->majorsFixture->records[0];
+        $url='/majors/view/' . $record2View['id'];
+        $html=$this->loginRequestResponse(FixtureConstants::userAndyAdminId,$url);
 
-        // Parse the html from the response
-        $html = str_get_html($this->_response->body());
-
-        // 1.  Look for the table that contains the view fields.
+        // 2.  Look for the table that contains the view fields.
         $this->table = $html->find('table#MajorViewTable',0);
         $this->assertNotNull($this->table);
 
-        // 2. Now inspect the fields on the form.  We want to know that:
+        // 3. Now inspect the fields on the form.  We want to know that:
         // A. The correct fields are there and no other fields.
         // B. The fields have correct values.
         //
@@ -281,24 +241,23 @@ class MajorsControllerTest extends DMIntegrationTestCase {
         // This is the count of the table rows that are presently unaccounted for.
         $unknownRowCnt = count($this->table->find('tr'));
 
-        // 2.1 title
+        // 3.1 title
         $field = $html->find('tr#title td',0);
-        $this->assertEquals($this->majorsFixture->major1Record['title'], $field->plaintext);
+        $this->assertEquals($record2View['title'], $field->plaintext);
         $unknownRowCnt--;
 
-        // 2.2 sdesc
+        // 3.2 sdesc
         $field = $html->find('tr#sdesc td',0);
-        $this->assertEquals($this->majorsFixture->major1Record['sdesc'], $field->plaintext);
+        $this->assertEquals($record2View['sdesc'], $field->plaintext);
         $unknownRowCnt--;
 
         // Have all the rows been accounted for?  Are there any extras?
         $this->assertEquals(0, $unknownRowCnt);
 
-        // 3. Examine the <A> tags on this page.  There should be zero links.
+        // 4. Examine the <A> tags on this page.  There should be zero links.
         $this->content = $html->find('div#MajorsView',0);
         $this->assertNotNull($this->content);
         $links = $this->content->find('a');
         $this->assertEquals(0,count($links));
     }
-
 }
