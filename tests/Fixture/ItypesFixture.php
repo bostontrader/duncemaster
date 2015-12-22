0eba8f0554589f89829251b733a459cae3641a7b
tradloff <bostontrader@gmail.com>
2015-12-22 20:47:47 +0800

    Update Itypes testing.

--- tests/Fixture/ItypesFixture.php
+++ tests/Fixture/ItypesFixture.php
@@ -6,7 +6,7 @@ class ItypesFixture extends DMFixture {
 
     // These records are injected into the db before the tests.  We need to specify the
     // id to ensure the test records are properly related.
-    public $itypeAttendRecord = [
+    /*public $itypeAttendRecord = [
         'id'=>FixtureConstants::itypeAttend_id,
         'title' => 'Attend'
     ];
@@ -25,18 +25,12 @@ class ItypesFixture extends DMFixture {
     public $itypeExcusedAbsenceRecord = [
         'id'=>FixtureConstants::itypeExcusedAbsence_id,
         'title' => 'Participate'
-    ];
+    ];*/
     // This record will be added during a test.  We don't need or want to control the id here, so omit it.
-    public $newItypeRecord = ['title' => 'Massacre class'];
+    public $newItypeRecord = ['title' => 'Massacre classmates'];
 
     public function init() {
-        $this->records = [
-            $this->itypeAttendRecord,
-            $this->itypeEjectRecord,
-            $this->itypeLeaveRecord,
-            $this->itypeParticipateRecord,
-        ];
-        parent::init();
+        $this->tableName='Itypes';
+        parent::init(); // This is where the records are loaded.
     }
-
 }
--- tests/TestCase/Controller/ItypesControllerTest.php
+++ tests/TestCase/Controller/ItypesControllerTest.php
@@ -17,6 +17,7 @@ class ItypesControllerTest extends DMIntegrationTestCase {
     /* @var \App\Model\Table\ItypesTable */
     private $itypes;
 
+    /* @var \App\Test\Fixture\ItypesFixture */
     private $itypesFixture;
 
     public function setUp() {
@@ -39,166 +40,128 @@ class ItypesControllerTest extends DMIntegrationTestCase {
 
     public function testAddGET() {
 
-        // 1. Simulate login, submit request, examine response.
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->get('/itypes/add');
-        $this->assertResponseOk(); // 2xx
-        $this->assertNoRedirect();
+        // 1. Login, GET the url, parse the response and send it back.
+        $html=$this->loginRequestResponse(FixtureConstants::userAndyAdminId,'/itypes/add');
 
-        // 2. Parse the html from the response
-        $html = str_get_html($this->_response->body());
+        // 2. Ensure that the correct form exists
+        /* @var \simple_html_dom_node $form */
+        $form = $html->find('form#ItypeAddForm',0);
+        $this->assertNotNull($form);
 
-        // 3. Ensure that the correct form exists
-        $this->form = $html->find('form#ItypeAddForm',0);
-        $this->assertNotNull($this->form);
-
-        // 4. Now inspect the fields on the form.  We want to know that:
+        // 3. Now inspect the fields on the form.  We want to know that:
         // A. The correct fields are there and no other fields.
         // B. The fields have correct values. This includes verifying that select
         //    lists contain options.
         //
         //  The actual order that the fields are listed is hereby deemed unimportant.
 
-        // 4.1 These are counts of the select and input fields on the form.  They
+        // 3.1 These are counts of the select and input fields on the form.  They
         // are presently unaccounted for.
-        $unknownSelectCnt = count($this->form->find('select'));
-        $unknownInputCnt = count($this->form->find('input'));
+        $unknownSelectCnt = count($form->find('select'));
+        $unknownInputCnt = count($form->find('input'));
 
-        // 4.2 Look for the hidden POST input
-        if($this->lookForHiddenInput($this->form)) $unknownInputCnt--;
+        // 3.2 Look for the hidden POST input
+        if($this->lookForHiddenInput($form)) $unknownInputCnt--;
 
-        if($this->inputCheckerA($this->form,'input#ItypeTitle')) $unknownInputCnt--;
+        if($this->inputCheckerA($form,'input#ItypeTitle')) $unknownInputCnt--;
 
-        // 4.9 Have all the input and select fields been accounted for?  Are there
-        // any extras?
-        $this->assertEquals(0, $unknownInputCnt);
-        $this->assertEquals(0, $unknownSelectCnt);
-
-        // 5. Examine the <A> tags on this page.  There should be zero links.
-        $this->content = $html->find('div#ItypesAdd',0);
-        $this->assertNotNull($this->content);
-        $links = $this->content->find('a');
-        $this->assertEquals(0,count($links));
+        // 4. Have all the input, select, and Atags been accounted for?
+        $this->expectedInputsSelectsAtagsFound($unknownInputCnt, $unknownSelectCnt, $html, 'div#ItypesAdd');
     }
 
     public function testAddPOST() {
 
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->post('/itypes/add', $this->itypesFixture->newItypeRecord);
-        $this->assertResponseSuccess(); // 2xx, 3xx
-        $this->assertRedirect( '/itypes' );
+        // 1. Login, POST a suitable record to the url, redirect, and return the record just
+        // posted, as read from the db.
+        $fixtureRecord=$this->itypesFixture->newItypeRecord;
+        $fromDbRecord=$this->genericAddPostProlog(
+            FixtureConstants::userAndyAdminId,
+            '/itypes/add', $fixtureRecord,
+            '/itypes', $this->itypes
+        );
 
-        // Now verify what we think just got written
-        $new_id = count($this->itypesFixture->records) + 1;
-        $query = $this->itypes->find()->where(['id' => $new_id]);
-        $this->assertEquals(1, $query->count());
-
-        // Now retrieve that 1 record and compare to what we expect
-        $new_itype = $this->itypes->get($new_id);
-        $this->assertEquals($new_itype['title'],$this->itypesFixture->newItypeRecord['title']);
+        // 2. Now validate that record.
+        $this->assertEquals($fromDbRecord['title'],$fixtureRecord['title']);
     }
 
     public function testDeletePOST() {
 
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $itype_id = $this->itypesFixture->itypeAttendRecord['id'];
-        $this->post('/itypes/delete/' . $itype_id);
-        $this->assertResponseSuccess(); // 2xx, 3xx
-        $this->assertRedirect( '/itypes' );
-
-        // Now verify that the record no longer exists
-        $query = $this->itypes->find()->where(['id' => $itype_id]);
-        $this->assertEquals(0, $query->count());
+        $itype_id = $this->itypesFixture->records[0]['id'];
+        $this->deletePOST(
+            FixtureConstants::userAndyAdminId, '/itypes/delete/',
+            $itype_id, '/itypes', $this->itypes
+        );
     }
 
     public function testEditGET() {
 
-        // 1. Simulate login, submit request, examine response.
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->get('/itypes/edit/' . $this->itypesFixture->itypeAttendRecord['id']);
-        $this->assertResponseOk(); // 2xx
-        $this->assertNoRedirect();
+        // 1. Obtain a record to edit, login, GET the url, parse the response and send it back.
+        $record2Edit=$this->itypesFixture->records[0];
+        $url='/itypes/edit/' . $record2Edit['id'];
+        $html=$this->loginRequestResponse(FixtureConstants::userAndyAdminId,$url);
 
-        // 2. Parse the html from the response
-        $html = str_get_html($this->_response->body());
-
-        // 3. Ensure that the correct form exists
-        $this->form = $html->find('form#ItypeEditForm',0);
-        $this->assertNotNull($this->form);
-
-        // 4. Now inspect the fields on the form.  We want to know that:
+        // 2. Ensure that the correct form exists
+        /* @var \simple_html_dom_node $form */
+        $form = $html->find('form#ItypeEditForm',0);
+        $this->assertNotNull($form);
+        
+        // 3. Now inspect the fields on the form.  We want to know that:
         // A. The correct fields are there and no other fields.
         // B. The fields have correct values. This includes verifying that select
         //    lists contain options.
         //
         //  The actual order that the fields are listed on the form is hereby deemed unimportant.
 
-        // 4.1 These are counts of the select and input fields on the form.  They
+        // 3.1 These are counts of the select and input fields on the form.  They
         // are presently unaccounted for.
-        $unknownSelectCnt = count($this->form->find('select'));
-        $unknownInputCnt = count($this->form->find('input'));
+        $unknownSelectCnt = count($form->find('select'));
+        $unknownInputCnt = count($form->find('input'));
 
-        // 4.2 Look for the hidden POST input
-        if($this->lookForHiddenInput($this->form,'_method','PUT')) $unknownInputCnt--;
+        // 3.2 Look for the hidden POST input
+        if($this->lookForHiddenInput($form,'_method','PUT')) $unknownInputCnt--;
 
-        // 4.3 giv_name
-        if($this->inputCheckerA($this->form,'input#ItypeTitle',
-            $this->itypesFixture->itypeAttendRecord['title'])) $unknownInputCnt--;
+        // 3.3 title
+        if($this->inputCheckerA($form,'input#ItypeTitle',
+            $record2Edit['title'])) $unknownInputCnt--;
 
-        // 4.9 Have all the input and select fields been accounted for?  Are there
-        // any extras?
-        $this->assertEquals(0, $unknownInputCnt);
-        $this->assertEquals(0, $unknownSelectCnt);
-
-        // 5. Examine the <A> tags on this page.  There should be zero links.
-        $this->content = $html->find('div#ItypesEdit',0);
-        $this->assertNotNull($this->content);
-        $links = $this->content->find('a');
-        $this->assertEquals(0,count($links));
+        // 5. Have all the input, select, and Atags been accounted for?
+        $this->expectedInputsSelectsAtagsFound($unknownInputCnt, $unknownSelectCnt, $html, 'div#ItypesEdit');
     }
 
     public function testEditPOST() {
 
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $itype_id = $this->itypesFixture->itypeAttendRecord['id'];
-        $this->put('/itypes/edit/' . $itype_id, $this->itypesFixture->newItypeRecord);
-        $this->assertResponseSuccess(); // 2xx, 3xx
-        $this->assertRedirect('/itypes');
+        // 1. Login, POST a suitable record to the url, redirect, and return the record just
+        // posted, as read from the db.
+        $fixtureRecord=$this->itypesFixture->newItypeRecord;
+        $fromDbRecord=$this->genericEditPutProlog(
+            FixtureConstants::userAndyAdminId,
+            '/itypes/edit', $fixtureRecord,
+            '/itypes', $this->itypes
+        );
 
-        // Now verify what we think just got written
-        $query = $this->itypes->find()->where(['id' => $itype_id]);
-        $this->assertEquals(1, $query->count());
-
-        // Now retrieve that 1 record and compare to what we expect
-        $itype = $this->itypes->get($itype_id);
-        $this->assertEquals($itype['title'],$this->itypesFixture->newItypeRecord['title']);
+        // 2. Now validate that record.
+        $this->assertEquals($fromDbRecord['title'],$fixtureRecord['title']);
     }
 
     public function testIndexGET() {
 
-        // 1. Simulate login, submit request, examine response.
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->get('/itypes/index');
-        $this->assertResponseOk(); // 2xx
-        $this->assertNoRedirect();
+        // 1. Login, GET the url, parse the response and send it back.
+        $html=$this->loginRequestResponse(FixtureConstants::userAndyAdminId,'/itypes/index');
 
-        // 2. Parse the html from the response
-        $html = str_get_html($this->_response->body());
-
-        // 3. Get a the count of all <A> tags that are presently unaccounted for.
+        // 2. Get a the count of all <A> tags that are presently unaccounted for.
         $this->content = $html->find('div#ItypesIndex',0);
         $this->assertNotNull($this->content);
         $unknownATag = count($this->content->find('a'));
 
-        // 4. Look for the create new itype link
+        // 3. Look for the create new itype link
         $this->assertEquals(1, count($html->find('a#ItypeAdd')));
         $unknownATag--;
 
-        // 5. Ensure that there is a suitably named table to display the results.
+        // 4. Ensure that there is a suitably named table to display the results.
         $this->table = $html->find('table#itypes',0);
         $this->assertNotNull($this->table);
 
-        // 6. Ensure that said table's thead element contains the correct
+        // 5. Ensure that said table's thead element contains the correct
         //    headings, in the correct order, and nothing else.
         $this->thead = $this->table->find('thead',0);
         $thead_ths = $this->thead->find('tr th');
@@ -208,13 +171,13 @@ class ItypesControllerTest extends DMIntegrationTestCase {
         $column_count = count($thead_ths);
         $this->assertEquals($column_count,2); // no other columns
 
-        // 7. Ensure that the tbody section has the same
+        // 6. Ensure that the tbody section has the same
         //    quantity of rows as the count of itype records in the fixture.
         $this->tbody = $this->table->find('tbody',0);
         $tbody_rows = $this->tbody->find('tr');
         $this->assertEquals(count($tbody_rows), count($this->itypesFixture->records));
 
-        // 8. Ensure that the values displayed in each row, match the values from
+        // 7. Ensure that the values displayed in each row, match the values from
         //    the fixture.  The values should be presented in a particular order
         //    with nothing else thereafter.
         $iterator = new \MultipleIterator();
@@ -228,7 +191,7 @@ class ItypesControllerTest extends DMIntegrationTestCase {
 
             $this->assertEquals($fixtureRecord['title'],  $htmlColumns[0]->plaintext);
 
-            // 8.2 Now examine the action links
+            // 7.1 Now examine the action links
             $this->td = $htmlColumns[1];
             $actionLinks = $this->td->find('a');
             $this->assertEquals('ItypeView', $actionLinks[0]->name);
@@ -238,51 +201,47 @@ class ItypesControllerTest extends DMIntegrationTestCase {
             $this->assertEquals('ItypeDelete', $actionLinks[2]->name);
             $unknownATag--;
 
-            // 8.9 No other columns
+            // 7.9 No other columns
             $this->assertEquals(count($htmlColumns),$column_count);
         }
 
-        // 9. Ensure that all the <A> tags have been accounted for
+        // 8. Ensure that all the <A> tags have been accounted for
         $this->assertEquals(0, $unknownATag);
     }
 
     public function testViewGET() {
 
-        $this->fakeLogin(FixtureConstants::userAndyAdminId);
-        $this->get('/itypes/view/' . $this->itypesFixture->itypeAttendRecord['id']);
-        $this->assertResponseOk(); // 2xx
-        $this->assertNoRedirect();
+        // 1. Obtain a record to view, login, GET the url, parse the response and send it back.
+        $record2View=$this->itypesFixture->records[0];
+        $url='/itypes/view/' . $record2View['id'];
+        $html=$this->loginRequestResponse(FixtureConstants::userAndyAdminId,$url);
 
-        // Parse the html from the response
-        $html = str_get_html($this->_response->body());
+        // 2.  Look for the table that contains the view fields.
+        $this->table = $html->find('table#ItypeViewTable',0);
+        $this->assertNotNull($this->table);
 
-        // 1.  Look for the table that contains the view fields.
-        $table = $html->find('table#ItypeViewTable',0);
-        $this->assertNotNull($table);
-
-        // 2. Now inspect the fields on the form.  We want to know that:
+        // 3. Now inspect the fields on the form.  We want to know that:
         // A. The correct fields are there and no other fields.
         // B. The fields have correct values.
         //
         //  The actual order that the fields are listed is hereby deemed unimportant.
 
         // This is the count of the table rows that are presently unaccounted for.
-        $unknownRowCnt = count($table->find('tr'));
+        $unknownRowCnt = count($this->table->find('tr'));
 
-        // 2.1 title
+        // 3.1 title
         $field = $html->find('tr#title td',0);
-        $this->assertEquals($this->itypesFixture->itypeAttendRecord['title'], $field->plaintext);
+        $this->assertEquals($record2View['title'], $field->plaintext);
         $unknownRowCnt--;
 
-        // Have all the rows been accounted for?  Are there
+        // 3.9 Have all the rows been accounted for?  Are there
         // any extras?
         $this->assertEquals(0, $unknownRowCnt);
 
-        // 3. Examine the <A> tags on this page.  There should be zero links.
+        // 4. Examine the <A> tags on this page.  There should be zero links.
         $this->content = $html->find('div#ItypesView',0);
         $this->assertNotNull($this->content);
         $links = $this->content->find('a');
         $this->assertEquals(0,count($links));
     }
-
 }
