678b317f20942fc95fd5c3937a1ce4c9fc5760a0
Thomas Radloff <bostontrader@gmail.com>
2016-03-03 06:52:05 +0800

    Work on attendence form.

--- src/Controller/SectionsController.php
+++ src/Controller/SectionsController.php
@@ -45,15 +45,7 @@ class SectionsController extends AppController {
         $section = $this->Sections->get($id,['contain' => ['Cohorts.Majors','Semesters','Subjects','Teachers']]);
 
 
-        // WARNING: SQL Injection risk!
-        // This query collects the students names and sids, in sid order. Other queries collect
-        // other info, also in sid order. Be careful to ensure that each query contains
-        // exactly the same students.
-        // |-------------------|-------------------|--------------|
-        // | students.fam_name | students.giv_name | students.sid |
-        // section 2, cohort 3
-
-
+        // 2. And we'll need some info about the students.
         /* @var \Cake\Database\Connection $connection */
         $connection = ConnectionManager::get('default');
         $query = "SELECT sid, fam_name, giv_name from students
@@ -80,8 +72,6 @@ class SectionsController extends AppController {
         $attendResults = $connection->execute($query)->fetchAll('assoc');*/
 
 
-
-
         // The primary method of specifying position is to measure mm from the origin,
         // where the origin is at the upper-left corner of the paper, moving right increases x
         // and moving down increases y. We will also use a 2nd level of positioning, described
@@ -95,168 +85,10 @@ class SectionsController extends AppController {
         $pdf->setPrintFooter(false);
 
         $pdf->AddPage();
-        $this->emitAttendForm($pdf,$section,$classRoster);
+        $this->emitAttendForm($pdf,$section,$classRoster,1);
 
         $pdf->AddPage();
-        $this->emitAttendForm($pdf,$section,$classRoster);
-
-        // 2. Lǚyóu zhíyè xuéyuàn kǎopíng dēngjì biǎo
-        // Tourism College Evaluation Registration Form
-        //$pdf->SetFontSize(22);
-        //$pdf->SetXY(100,10);
-        //$pdf->Cell(100,0,'山东旅游职业学院考评登记表',0,0,'C');
-
-        // 3. xue qi semester
-        //$pdf->SetFontSize(12);
-        //$pdf->SetXY(236,16);
-        //$pdf->Cell(0,0,'学期:');
-        //$pdf->SetXY(249,15);
-        //$pdf->Cell(0,0,$section->semester->nickname);
-        //$pdf->Line(248,21,273,21); // h
-
-        // 4.
-        //$pdf->SetXY(30,22);
-        //$pdf->SetFontSize(17);
-        //$pdf->Cell(0,0,'出勤,作业');
-
-        //$pdf->SetFontSize(14);
-        //$pdf->SetXY(57,24);
-        //$pdf->Cell(0,0,'(注: 事假: X 旷课: Ø 病假: / 迟到: O 早退: + 迟到又早退: ⊕ 公假: ∆ 正常或完成作业: ✓ , 未完成作业: X)');
-
-        // 5. zhōu cì week
-        //$pdf->SetFontSize(12);
-        //$pdf->SetXY(32,30);
-        //$pdf->Cell(0,0,'周次');
-
-        // 6. Xùhào No.
-        //$pdf->SetXY(19,37);
-        //$pdf->Cell(0,0,'序');
-        //$pdf->SetXY(19,42);
-        //$pdf->Cell(0,0,'号');
-
-        // 7. xué hào Student Id
-        //$pdf->SetXY(26,40);
-        //$pdf->Cell(0,0,'学号');
-
-        // 8. xìngmíng full name
-        //$pdf->SetXY(39,40);
-        //$pdf->Cell(0,0,'姓名');
-
-        // 9. Kèchéng míngchēng Course title
-        //$pdf->SetXY(26,194);
-        //$pdf->Cell(0,0,'课程名称:');
-        //$pdf->SetXY(48,193);
-        //$pdf->Cell(0,0,$section->subject->title);
-        //$pdf->Line(47,199,80,199); // h
-
-        // 10.Bānjí class
-        //$pdf->SetXY(99,194);
-        //$pdf->Cell(0,0,'班级:');
-        //$pdf->SetXY(112,193);
-        //$n=$section->cohort->nickname;
-        //$pdf->Cell(0,0,$n);
-        //$pdf->Line(111,199,136,199); // h
-
-        // 11.Rènkè lǎoshī Instructor
-        //$pdf->SetXY(162,194);
-        //$pdf->Cell(0,0,'任课老师:');
-        //$pdf->SetXY(184,193);
-        //$pdf->Cell(0,0,$section->teacher->fam_name);
-        //$pdf->Line(183,199,209,199); // h
-
-        // In addition to the fundamental system of positioning, as described
-        // earlier, we also use a 2nd level positioning system.
-        //
-        // The main body of the form contains an array of 30 rows x 27 columns.
-        // As an aid to the subsequent population of this form, we will build two
-        // data structures $cx and $cy which together contain the level 1 coordinates (x,y in mm) for
-        // each of the level two elements. We can then print any desired element using
-        // this higher level of abstraction.
-        //
-        // $c is structured as follows:
-        // $cx['a'], $cx['b'], $cx['c'],
-        // $cx['weeks'][w]['a'] or ['b'] where w = 0-11
-        // where the content is a numeric x value
-        //
-        // $c[y] where y = 0 to 29
-        // where the content is a numeric y value
-        //
-
-        // 12. Draw the main outerbox
-        //$leftX = 19;
-        //$rightX = 279;
-        //$topY = 30;
-        //$bottomY = 191;
-        //$pdf->SetLineWidth(0.5);
-        //$pdf->Line($leftX,$topY,$leftX,$bottomY); // v
-        //$pdf->Line($rightX,$topY,$rightX,$bottomY); // v
-        //$pdf->Line($leftX,$topY,$rightX,$topY); // h
-        //$pdf->Line($leftX,$bottomY,$rightX,$bottomY); // h
-        //$cx['a']=$leftX;
-
-        // Left most inner vertical lines
-        //$pdf->SetLineWidth(0.1);
-        //$cx['b']=26;
-        //$pdf->Line($cx['b'],35,$cx['b'],$bottomY); // v
-        //$cx['c']=36;
-        //$pdf->Line($cx['c'],35,$cx['c'],$bottomY); // v
-
-        // Top most inner lines
-        //$pdf->Line($leftX,35,$rightX,35); // h
-        //$pdf->Line(54,45,$rightX,45); // h
-
-
-
-        // The horizontal lines
-        //for($y=0, $yidx=0; $y<138; $y+=4.71) {
-            //$y1=$y+49.5;
-            //$cy[$yidx++]=$y1;
-            //$pdf->Line($leftX,$y1,$rightX,$y1); // h
-        //}
-
-        // Vertical lines
-        //$priorX1=0;
-        //for($x=0, $widx=0; $x<225; $x+=18.8) {
-            //$x1=$x+54;
-            //$cx['weeks'][$widx]['a']=$x1;
-            //$cx['weeks'][$widx]['b']=$x1+9.4;
-            //$pdf->Line($x1,$topY,$x1,$bottomY); // v
-            //$pdf->Line($x1+9.4,$topY+15,$x1+9.4,$bottomY); // v
-            //$widx++;
-
-            //$pdf->SetXY($x1,$topY);
-            //$pdf->Cell(18.8,0,"第 $widx 周",0,0,'C');
-
-
-            //if($x>0) {
-                //$pdf->Line($priorX1,45,$x1,35); // sloped
-            //}
-            //$priorX1=$x1;
-        //}
-        //$pdf->Line($priorX1,45,$rightX,35); // sloped
-
-        // Fill in the form
-        //foreach($cy as $yidx=>$y2) {
-            //$pdf->SetXY($cx['a'],$y2);
-            //$pdf->Cell(7,0,$yidx+1,0,0,'C');    // sequence no
-
-            //$pdf->SetXY($cx['b']+1.0,$y2);
-            //$sid=substr($classRoster[$yidx]['sid'],-4);
-            //$pdf->Cell(10,0,$sid,0,0,'R');
-
-            //$pdf->SetXY($cx['c'],$y2);
-            //$fullName=$classRoster[$yidx]['fam_name'].$classRoster[$yidx]['giv_name'];
-            //$pdf->Cell(20,0,$fullName,0,0,'L');
-
-            //for($i=0; $i<=11; $i++) {
-                //$pdf->SetXY($cx['weeks'][$i]['a'],$y2);
-                //$pdf->Cell(9,0,'aaa',0,0,'C');
-
-                //$pdf->SetXY($cx['weeks'][$i]['b'],$y2);
-                //$pdf->Cell(9,0,'bbb',0,0,'C');
-            //}
-        //}
-
+        $this->emitAttendForm($pdf,$section,$classRoster,31);
 
         $pdf->Output();
         $this->response->type('application/pdf');
@@ -264,31 +96,6 @@ class SectionsController extends AppController {
     }
 
     private function emitAttendForm($pdf,$section,$classRoster) {
-
-        //select teachers.fam_name, teachers.giv_name, subjects.title
-        //from sections
-        //left join teachers on sections.teacher_id = teachers.id
-        //left join subjects on sections.subject_id = subjects.id
-        //$section = $this->Sections->get($id,['contain' => ['Cohorts.Majors','Semesters','Subjects','Teachers']]);
-
-        // 1. Get started
-        //require('tcpdf.php');
-
-        // The primary method of specifying position is to measure mm from the origin,
-        // where the origin is at the upper-left corner of the paper, moving right increases x
-        // and moving down increases y. We will also use a 2nd level of positioning, described
-        // shortly, on top of this level one positioning.
-
-        //$pdf = new \tcpdf('L','mm','a4');
-        //$pdf->SetFont('cid0cs', '', 16, '', true);
-
-        // remove default header/footer
-        //$pdf->setPrintHeader(false);
-        //$pdf->setPrintFooter(false);
-
-        //$pdf->AddPage();
-
-        //$this->emitAttendForm();
 
         // 1. Lǚyóu zhíyè xuéyuàn kǎopíng dēngjì biǎo
         // Tourism College Evaluation Registration Form
@@ -437,10 +244,10 @@ class SectionsController extends AppController {
 
             for($i=0; $i<=11; $i++) {
                 $pdf->SetXY($cx['weeks'][$i]['a'],$y2);
-                $pdf->Cell(9,0,'aaa',0,0,'C');
+                //$pdf->Cell(9,0,'aaa',0,0,'C');
 
                 $pdf->SetXY($cx['weeks'][$i]['b'],$y2);
-                $pdf->Cell(9,0,'bbb',0,0,'C');
+                //$pdf->Cell(9,0,'bbb',0,0,'C');
             }
         }
 
