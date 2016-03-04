e8b445bba4e15997da645a384d74d78051bef496
Thomas Radloff <bostontrader@gmail.com>
2016-03-04 18:11:00 +0800

    Work on teaching plan form. Work in progress. Way ugly!

--- src/Controller/TplansController.php
+++ src/Controller/TplansController.php
@@ -58,7 +58,14 @@ class TplansController extends AppController {
     // in booklet form.  Hence the quantity of document pages should be a multiple of 4.
     public function pdf($id = null) {
 
-        // 1. Get started
+        // 1. In the beginning...
+        // 1.1 Obtain the data to print.
+        $info['subject']='Intro to Warp Theory';
+        $info['major']='Warp Drive Engineering';
+        $info['cohorts']='15A1, 15A2';
+        $info['instructor']='Spock';
+
+        // 1.2 Initialize the pdf
         require('tcpdf.php');
 
         // The primary method of specifying position is to measure mm from the origin,
@@ -72,12 +79,164 @@ class TplansController extends AppController {
         $pdf->setPrintHeader(false);
         $pdf->setPrintFooter(false);
 
+        // 2. Page 1, The cover
+        $pdf->AddPage();
+
+        // 2.1 shandong Lǚyóu zhíyè xuéyuàn
+        // Shandong College of Tourism and Hospitality
+        $pdf->SetFontSize(21);
+        $pdf->SetXY(44,20);
+        $pdf->Cell(74,8,'山 东 旅 游 职 业 学 院',1,0,'C');
+
+        // 2.2 jia4oxue2 ri4li4
+        // Teaching plan
+        $pdf->SetFontSize(40);
+        $pdf->SetXY(21,37);
+        $pdf->Cell(123,18,'教     学     日     历',1,0,'C');
+
+        // 2.3 ke4che2ng mi2ngche1ng
+        // Course title
+        $pdf->SetFontSize(16);
+        $pdf->SetXY(37,77);
+        $pdf->Cell(22,8,'课程名称',1,0,'C');
+
+        // 2.4 zhua1n ye4
+        // Profession
+        $pdf->SetXY(37,92);
+        $pdf->Cell(11,8,'专业',1,0,'C');
+
+        // nia2n ji2
+        // Grade aka cohorts
+        $pdf->SetXY(82,92);
+        $pdf->Cell(12,8,'年级',1,0,'C');
+
+        // zh3jia3ng jia4o shi1
+        // Speaker/teacher
+        $pdf->SetFontSize(12);
+        $pdf->SetXY(37,102);
+        $pdf->Cell(8,5,'主讲',1,0,'C');
+        $pdf->SetXY(37,107);
+        $pdf->Cell(8,5,'教师',1,0,'C');
+
+        // xi4ng mi2ng
+        // Full name
+        $pdf->SetFontSize(16);
+        $pdf->SetXY(47,103);
+        $pdf->Cell(11,8,'姓名',1,0,'C');
+
+        // zhi2 che1ng
+        // Job title
+        $pdf->SetXY(87,103);
+        $pdf->Cell(12,8,'职称',1,0,'C');
+
+        $pdf->SetXY(37,129);
+        $pdf->Cell(17,7,'周   数',1,0,'C');
+        //$pdf->SetXY(100,100);
+        //$pdf->Cell(100,0,'数',0,0,'C');
+
+
+        $pdf->SetXY(37,138);
+        $pdf->Cell(17,7,'讲   课',1,0,'C');
+        //$pdf->SetXY(100,120);
+        //$pdf->Cell(100,0,'课',0,0,'C');
+
+        $pdf->SetXY(106,138);
+        $pdf->Cell(13,7,'学时',1,0,'C');
+
+
+        $pdf->SetXY(37,147);
+        $pdf->Cell(17,7,'实习课',1,0,'C');
+        $pdf->SetXY(106,147);
+        $pdf->Cell(13,7,'学时',1,0,'C');
+
+
+        $pdf->SetXY(37,156);
+        $pdf->Cell(17,7,'实   验',1,0,'C');
+        //$pdf->SetXY(100,170);
+        //$pdf->Cell(100,0,'验',0,0,'C');
+        $pdf->SetXY(106,156);
+        $pdf->Cell(13,7,'学时',1,0,'C');
+
+
+        $pdf->SetXY(37,165);
+        $pdf->Cell(17,7,'总   计',1,0,'C');
+        //$pdf->SetXY(100,190);
+        //$pdf->Cell(100,0,'计',0,0,'C');
+        $pdf->SetXY(106,165);
+        $pdf->Cell(13,7,'学时',1,0,'C');
+
+
+        $pdf->Line(59,83, 118,83); // h
+        $pdf->Line(48,97, 84, 97); // h
+        $pdf->Line(95,97, 118,97); // h
+        $pdf->Line(57,110, 88,110); // h
+        $pdf->Line(98,110, 118,110); // h
+
+        $pdf->Line(53,135,118,135); // h
+        $pdf->Line(53,144,108,144); // h
+        $pdf->Line(53,153,108,153); // h
+        $pdf->Line(53,162,108,162); // h
+        $pdf->Line(53,172,108,172); // h
+
+        $pdf->SetXY(34,204);
+        $pdf->Cell(30,7,'2015-2016',1,0,'C');
+        $pdf->SetXY(80,204);
+        $pdf->Cell(21,7,'学年第',1,0,'C');
+        $pdf->SetXY(109,204);
+        $pdf->Cell(11,7,'学期',1,0,'C');
+
+
+        // 2. The Plan Elements
         $pdf->AddPage();
 
 
-        // 1. Page 1, The cover
 
-        // 2. The Plan Elements
+
+        // Draw the header box
+        $offsetX=5;
+        $offsetY=5;
+        $leftX = 0;
+        $rightX = 154;
+        $topY = 0;
+        $bottomY = 37;
+        $pdf->SetLineWidth(0.5);
+        $pdf->Line($leftX+$offsetX, $topY+$offsetY,   $leftX+$offsetX, $bottomY+$offsetY); // v
+        $pdf->Line($rightX+$offsetX,$topY+$offsetY,   $rightX+$offsetX,$bottomY+$offsetY); // v
+        $pdf->Line($leftX+$offsetX, $topY+$offsetY,   $rightX+$offsetX,$topY+$offsetY); // h
+        $pdf->Line($leftX+$offsetX, $bottomY+$offsetY,$rightX+$offsetX,$bottomY+$offsetY); // h
+
+        // Draw the vertical lines
+        $pdf->Line($leftX+18+$offsetX, $topY+$offsetY,   $leftX+18+$offsetX, $bottomY+$offsetY); // v
+        $pdf->Line($leftX+41+$offsetX, $topY+$offsetY,   $leftX+41+$offsetX, $bottomY+$offsetY); // v
+        $pdf->Line($leftX+89+$offsetX, $topY+$offsetY,   $leftX+89+$offsetX, $bottomY+$offsetY); // v
+        $pdf->Line($leftX+137+$offsetX, $topY+$offsetY,   $leftX+137+$offsetX, $bottomY+$offsetY); // v
+
+        $pdf->SetXY(5,14);
+        $pdf->Cell(5,7,'月',1,0,'C');
+        $pdf->SetXY(5,20);
+        $pdf->Cell(5,7,'份',1,0,'C');
+
+        $pdf->SetXY(35,16);
+        $pdf->Cell(15,7,'周   次',1,0,'C');
+        //$pdf->SetXY(100,40);
+        //$pdf->Cell(100,0,'次',0,0,'C');
+
+        $pdf->SetXY(54,13);
+        $pdf->Cell(20,7,'讲    课',1,0,'C');
+        //$pdf->SetXY(100,60);
+        //$pdf->Cell(100,0,'课',0,0,'C');
+        $pdf->SetXY(47,19);
+        $pdf->Cell(34,7,'（教学大纲章节题目）',1,0,'C');
+
+        $pdf->SetXY(95,13);
+        $pdf->Cell(34,7,'实验习题课课堂',1,0,'C');
+        $pdf->SetXY(95,19);
+        $pdf->Cell(34,7,'讨论及其它作业题目',1,0,'C');
+
+        $pdf->SetXY(143,11);
+        $pdf->Cell(5,7,'课',1,0,'C');
+        $pdf->SetXY(143,21);
+        $pdf->Cell(5,7,'时',1,0,'C');
 
         // The Plan Elements are each printed across one side of two consecutive pages.
 
@@ -106,32 +265,70 @@ class TplansController extends AppController {
         }
 
         // Emit the next 4 (plan elements or blank elements)
+        for($i=0; $i<4; $i++) {
+            $offsetY2=37+$i*36;
+            $pdf->Line($leftX+$offsetX, $topY+$offsetY+$offsetY2,   $leftX+$offsetX, $bottomY+$offsetY+$offsetY2); // v
+            $pdf->Line($rightX+$offsetX,$topY+$offsetY+$offsetY2,   $rightX+$offsetX,$bottomY+$offsetY+$offsetY2); // v
+            $pdf->Line($leftX+$offsetX, $topY+$offsetY+$offsetY2,   $rightX+$offsetX,$topY+$offsetY+$offsetY2); // h
+            $pdf->Line($leftX+$offsetX, $bottomY+$offsetY+$offsetY2,$rightX+$offsetX,$bottomY+$offsetY+$offsetY2); // h
+
+            // Draw the vertical lines
+            $pdf->Line($leftX+18+$offsetX, $topY+$offsetY+$offsetY2,   $leftX+18+$offsetX, $bottomY+$offsetY+$offsetY2); // v
+            $pdf->Line($leftX+41+$offsetX, $topY+$offsetY+$offsetY2,   $leftX+41+$offsetX, $bottomY+$offsetY+$offsetY2); // v
+            $pdf->Line($leftX+89+$offsetX, $topY+$offsetY+$offsetY2,   $leftX+89+$offsetX, $bottomY+$offsetY+$offsetY2); // v
+            $pdf->Line($leftX+137+$offsetX, $topY+$offsetY+$offsetY2,   $leftX+137+$offsetX, $bottomY+$offsetY+$offsetY2); // v            
+
+
+            $pdf->SetXY(20,17+$offsetY+$offsetY2);
+            $pdf->Cell(13,7,'第一周',1,0,'C');
+            $pdf->SetXY(20,23+$offsetY+$offsetY2);
+            $pdf->Cell(20,7,'从   到',1,0,'C');
+
+        }
+
         // Emit the signature block
 
+
+
+
+        $pdf->AddPage();
+
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'阅读主要参考书',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'辅导形式和主要内容',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'课时',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'备注',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'主讲老师',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'教研室主任',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'系主任',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'日期年月日',0,0,'C');
+
         // 2. Rear cover
+        $pdf->AddPage();
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'检查本日历完成情况的结果',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'检查者',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'主讲教师',0,0,'C');
+        $pdf->SetXY(100,80);
+        $pdf->Cell(100,0,'附注: 本日历一式三份, 一份存教务处,一份存教研室, 一份讲授者保存',0,0,'C');
 
 
 
 
 
-        // Draw the main outerbox
-        $offsetX=5;
-        $offsetY=5;
-        $leftX = 0;
-        $rightX = 154;
-        $topY = 0;
-        $bottomY = 219;
-        $pdf->SetLineWidth(0.5);
-        $pdf->Line($leftX+$offsetX, $topY+$offsetY,   $leftX+$offsetX, $bottomY+$offsetY); // v
-        $pdf->Line($rightX+$offsetX,$topY+$offsetY,   $rightX+$offsetX,$bottomY+$offsetY); // v
-        $pdf->Line($leftX+$offsetX, $topY+$offsetY,   $rightX+$offsetX,$topY+$offsetY); // h
-        $pdf->Line($leftX+$offsetX, $bottomY+$offsetY,$rightX+$offsetX,$bottomY+$offsetY); // h
 
-        // Draw the vertical lines
-        $pdf->Line($leftX+18+$offsetX, $topY+$offsetY,   $leftX+18+$offsetX, $bottomY+$offsetY); // v
-        $pdf->Line($leftX+41+$offsetX, $topY+$offsetY,   $leftX+41+$offsetX, $bottomY+$offsetY); // v
-        $pdf->Line($leftX+89+$offsetX, $topY+$offsetY,   $leftX+89+$offsetX, $bottomY+$offsetY); // v
-        $pdf->Line($leftX+137+$offsetX, $topY+$offsetY,   $leftX+137+$offsetX, $bottomY+$offsetY); // v
+
+
+
 
         // Draw the horizontal lines
         $pdf->Line($leftX+$offsetX, $topY+37+$offsetY,   $rightX+$offsetX,$topY+37+$offsetY); // h
